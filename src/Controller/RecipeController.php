04a6e83982f51fea2333fe76d9bde999a7042fac
ichibo <chibo@auxmoney.com>
2019-03-12 22:04:44 +0100

    WIC-I3: adding controller

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -5,13 +5,26 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
+    /*
+     * Route Options
+     * PUBLIC
+     * 1 - get recipe by id (uuid)
+     * 2 - get recipes based on search term (ingredient and recipe)
+     *
+     * PRIVATE - control using aop
+     * 1 - get all recipes
+     * 2 - add new recipe
+     * 3 - edit existing recipe
+     *
+     */
     /**
-     * @Route("/recipe/list", name="recipe")
+     * @Route("/get/recipe", name="recipe")
      */
     public function recipeAction(): Response
     {
@@ -21,8 +34,8 @@ class RecipeController extends AbstractController
     /**
      * @Route("/recipe/add", name="add-recipe")
      */
-    public function addAction(): Response
+    public function addAction(Request $request): Response
     {
-        return $this->render('Recipe/show.html.twig');
+        return $this->render('Recipe/add.html.twig');
     }
 }
