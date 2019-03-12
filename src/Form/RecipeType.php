da0bfbf602cf8abac69bbe31c45571f8dc9308b6
ichibo <chibo@auxmoney.com>
2019-03-12 22:04:08 +0100

    WIC-I2: preparing form

---
+++ src/Form/RecipeType.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('recipeName', TextType::class)
+        ;
+    }
+}
