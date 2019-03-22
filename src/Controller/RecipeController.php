841b53a0d15c3ca0b11bced80a6b7284690450d6
ichibo <chibo@auxmoney.com>
2019-03-22 11:29:46 +0100

    WIC-I4-display_recipe: adding action to controller to search by meal content

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\Request\MealContent;
+use App\DTO\Response\NotFound;
+use App\Form\RecipeType;
+use App\Service\RecipeView;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
+    private $recipeView;
+
+    public function __construct(RecipeView $recipeView)
+    {
+        $this->recipeView = $recipeView;
+    }
+
     /*
      * Route Options
      * PUBLIC
@@ -25,10 +37,40 @@ class RecipeController extends AbstractController
      */
     /**
      * @Route("/get/recipe", name="recipe")
+     *
+     * @throws Exception
      */
-    public function recipeAction(): Response
+    public function recipeBySearchTermAction(Request $request): Response
     {
-        return $this->render('Recipe/show.html.twig');
+        $mealContent = new MealContent();
+        $form = $this->createForm(RecipeType::class, $mealContent);
+
+        $form->handleRequest($request);
+
+        $params = ['status' => 'red'];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var MealContent $mealContent */
+            $mealContent = $form->getData();
+
+            $recipe = $this->recipeView->getRecipeByMealContent($mealContent->getMealContent());
+
+            if ($recipe instanceof NotFound){
+                $params['status'] = 'yellow';
+                $params['message'] = $recipe->getMessage();
+            }
+            else{
+
+                $params['status'] = 'green';
+                $recipes = [];
+                foreach ($recipe as $item) {
+                    $recipes[] = $item;
+                }
+                $params['recipes'] = $recipes;
+            }
+        }
+        return $this->render('Recipe/search.html.twig', ['searchForm' => $form->createView(), 'data' => $params]);
     }
 
     /**
