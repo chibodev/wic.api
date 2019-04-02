5af5a9a6dd5935e38e33d20a22b500ccb0887b2e
ichibo <chibo@auxmoney.com>
2019-04-02 02:17:53 +0200

    WIC-I3-adding_recipe: fixing unit test

--- src/Infrastructure/Tests/Unit/Service/RecipeViewServiceTest.php
+++ src/Infrastructure/Tests/Unit/Service/RecipeViewServiceTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Infrastructure\Tests\Unit\Service;
 
 use App\EntityInterface\RecipeInterface as RecipeEntityInterface;
+use App\Infrastructure\DTO\Response\RecipeShort;
 use App\Infrastructure\Entity\Recipe;
 use App\Infrastructure\Entity\Unknown;
 use App\Infrastructure\PublicInterface\DirectionRepositoryInterface;
@@ -12,7 +13,6 @@ use App\Infrastructure\PublicInterface\DTO\DirectionInterface;
 use App\Infrastructure\PublicInterface\DTO\IngredientInterface;
 use App\Infrastructure\PublicInterface\DTO\NotFoundInterface;
 use App\Infrastructure\PublicInterface\DTO\RecipeInterface;
-use App\Infrastructure\PublicInterface\DTO\RecipeShortInterface;
 use App\Infrastructure\PublicInterface\RecipeView;
 use App\Infrastructure\Repository\DirectionRepository;
 use App\Infrastructure\Repository\IngredientRepository;
@@ -24,13 +24,12 @@ use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 class RecipeViewServiceTest extends TestCase
 {
     /** @var NotFoundInterface|ObjectProphecy */
     private $notFound;
-    /** @var RecipeShortInterface|ObjectProphecy */
-    private $recipeShort;
     /** @var RecipeInterface|ObjectProphecy */
     private $recipeDto;
     /** @var RecipeRepository|ObjectProphecy */
@@ -55,11 +54,10 @@ class RecipeViewServiceTest extends TestCase
         $this->directionRepo = $this->prophesize(DirectionRepository::class);
         $this->logger = $this->prophesize(LoggerInterface::class);
         $this->notFound = $this->prophesize(NotFoundInterface::class);
-        $this->recipeShort = $this->prophesize(RecipeShortInterface::class);
         $this->recipeDto = $this->prophesize(RecipeInterface::class);
 
         $this->subject = new RecipeViewService($this->recipeRepo->reveal(), $this->unknownRepo->reveal(), $this->ingredientRepo->reveal(),
-            $this->directionRepo->reveal(), $this->notFound->reveal(), $this->recipeShort->reveal(), $this->recipeDto->reveal(),
+            $this->directionRepo->reveal(), $this->notFound->reveal(), $this->recipeDto->reveal(),
             $this->logger->reveal());
     }
 
@@ -100,12 +98,15 @@ class RecipeViewServiceTest extends TestCase
         $type = RecipeType::FOOD;
 
         $recipe = $this->prophesize(Recipe::class);
+        $recipeShort = $this->prophesize(RecipeShort::class);
+
         $recipe->getUuid()->shouldBeCalled()->willReturn('uuid');
         $recipe->getName()->shouldBeCalled()->willReturn('recipe');
         $recipe->getPrep()->shouldBeCalled()->willReturn(10);
         $recipe->getCook()->shouldBeCalled()->willReturn(20);
         $recipe->getType()->shouldBeCalled()->willReturn($type);
         $recipe->getImageUrl()->shouldBeCalled()->willReturn('url');
+        $recipe->isKeto()->shouldBeCalled()->willReturn(true);
 
         $this->recipeRepo->findByMealContent([$mealContent])->shouldBeCalled()->willReturn([$recipe->reveal()]);
 
@@ -114,12 +115,13 @@ class RecipeViewServiceTest extends TestCase
 
         $result = $this->subject->getRecipeByMealContent($mealContent);
 
-        $this->recipeShort->getUuid()->willReturn('uuid');
-        $this->recipeShort->getName()->willReturn('recipe');
-        $this->recipeShort->getPrep()->willReturn(10);
-        $this->recipeShort->getCook()->willReturn(20);
-        $this->recipeShort->getType()->willReturn($type);
-        $this->recipeShort->getImageUrl()->willReturn('url');
+        $recipeShort->getUuid()->willReturn('uuid');
+        $recipeShort->getName()->willReturn('recipe');
+        $recipeShort->getPrep()->willReturn(10);
+        $recipeShort->getCook()->willReturn(20);
+        $recipeShort->getType()->willReturn($type);
+        $recipeShort->getImageUrl()->willReturn('url');
+        $recipeShort->isKeto()->willReturn(true);
 
         self::assertIsArray($result);
         self::assertSame('uuid', $result[0]->getUuid());
@@ -128,6 +130,7 @@ class RecipeViewServiceTest extends TestCase
         self::assertSame(20, $result[0]->getCook());
         self::assertSame(RecipeType::FOOD, $result[0]->getType());
         self::assertSame('url', $result[0]->getImageUrl());
+        self::assertTrue($result[0]->isKeto());
     }
 
 
@@ -136,9 +139,10 @@ class RecipeViewServiceTest extends TestCase
         $uuid = 'invalid-uuid';
 
         $this->recipeRepo->findOneBy(Argument::any())->shouldNotBeCalled();
-        $result = $this->subject->getRecipeByUuid($uuid);
 
-        self::assertInstanceOf(NotFoundInterface::class, $result);
+        $this->expectException(RuntimeException::class);
+
+        $result = $this->subject->getRecipeByUuid($uuid);
     }
 
     public function testGetRecipeByUuidFails(): void
@@ -169,6 +173,7 @@ class RecipeViewServiceTest extends TestCase
         $recipe->getImageUrl()->shouldBeCalled()->willReturn('url');
         $recipe->getImageSource()->shouldBeCalled()->willReturn('urlSource');
         $recipe->getAuthor()->shouldBeCalled()->willReturn('me');
+        $recipe->isKeto()->shouldBeCalled()->willReturn(false);
 
         $direction->getDescription()->willReturn('description');
         $ingredient->getDescription()->willReturn('description');
@@ -189,6 +194,7 @@ class RecipeViewServiceTest extends TestCase
         $this->recipeDto->getAuthor()->willReturn('me');
         $this->recipeDto->getDirection()->willReturn([$direction->reveal()]);
         $this->recipeDto->getIngredient()->willReturn([$ingredient->reveal()]);
+        $this->recipeDto->isKeto()->willReturn(false);
 
         self::assertInstanceOf(RecipeInterface::class, $result);
         self::assertSame('recipe', $result->getName());
@@ -200,6 +206,6 @@ class RecipeViewServiceTest extends TestCase
         self::assertSame('me', $result->getAuthor());
         self::assertSame('description', $result->getDirection()[0]->getDescription());
         self::assertSame('description', $result->getIngredient()[0]->getDescription());
-
+        self::assertFalse($result->isKeto());
     }
 }
