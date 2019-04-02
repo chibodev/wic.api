bf62c012bc9de9cd30e47d07b0b2eb184a8da3ad
ichibo <chibo@auxmoney.com>
2019-04-02 22:53:47 +0200

    WIC-I10 Api Access Token: adding event for creating and saving new api user

---
+++ src/Infrastructure/Event/ApiUserEvent.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Event;
+
+use App\Common\App\Security\DTO\AccessKey;
+use App\Infrastructure\ValueObject\ApiUserRole;
+use Symfony\Component\EventDispatcher\Event;
+
+class ApiUserEvent extends Event
+{
+    private $uuid;
+    /** @var ApiUserRole */
+    private $role;
+    /** @var AccessKey */
+    private $accessKey;
+
+    public function __construct(string $uuid, ApiUserRole $role)
+    {
+        $this->uuid = $uuid;
+        $this->role = $role;
+    }
+
+    public function getApiUserUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getRole(): ApiUserRole
+    {
+        return $this->role;
+    }
+
+    public function getResult(): AccessKey
+    {
+        return $this->accessKey;
+    }
+
+    public function setAccessKey(AccessKey $accessKey): void
+    {
+        $this->accessKey = $accessKey;
+    }
+}
